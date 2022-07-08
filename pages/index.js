275aa3aa663cefddbc31330e92e6defdc196082c
Thanapoom Phithakjarukorn <thanapoomphithakjarukorn@Thanapooms-iMac.local>
2022-07-08 15:27:48 -0700

    custom _app.js and import css to use globally, refactor some code and make it render from server side

--- pages/index.js
+++ pages/index.js
@@ -1,54 +1,54 @@
-import Head from 'next/head'
-import clientPromise from '../lib/mongodb'
+import Head from 'next/head';
+import dbConnect from '../lib/dbConnect';
 
 export default function Home({ isConnected }) {
   return (
-    <div className="container">
+    <div className='container'>
       <Head>
         <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
 
       <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js with MongoDB!</a>
+        <h1 className='title'>
+          Welcome to <a href='https://nextjs.org'>Next.js with MongoDB!</a>
         </h1>
 
         {isConnected ? (
-          <h2 className="subtitle">You are connected to MongoDB</h2>
+          <h2 className='subtitle'>You are connected to MongoDB</h2>
         ) : (
-          <h2 className="subtitle">
+          <h2 className='subtitle'>
             You are NOT connected to MongoDB. Check the <code>README.md</code>{' '}
             for instructions.
           </h2>
         )}
 
-        <p className="description">
+        <p className='description'>
           Get started by editing <code>pages/index.js</code>
         </p>
 
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
+        <div className='grid'>
+          <a href='https://nextjs.org/docs' className='card'>
             <h3>Documentation &rarr;</h3>
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className="card">
+          <a href='https://nextjs.org/learn' className='card'>
             <h3>Learn &rarr;</h3>
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
           <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="card"
+            href='https://github.com/vercel/next.js/tree/canary/examples'
+            className='card'
           >
             <h3>Examples &rarr;</h3>
             <p>Discover and deploy boilerplate example Next.js projects.</p>
           </a>
 
           <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
+            href='https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
+            className='card'
           >
             <h3>Deploy &rarr;</h3>
             <p>
@@ -60,12 +60,12 @@ export default function Home({ isConnected }) {
 
       <footer>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
+          <img src='/vercel.svg' alt='Vercel Logo' className='logo' />
         </a>
       </footer>
 
@@ -219,12 +219,12 @@ export default function Home({ isConnected }) {
         }
       `}</style>
     </div>
-  )
+  );
 }
 
 export async function getServerSideProps(context) {
   try {
-    await clientPromise
+    await dbConnect();
     // `await clientPromise` will use the default database passed in the MONGODB_URI
     // However you can use another database (e.g. myDatabase) by replacing the `await clientPromise` with the following code:
     //
@@ -236,11 +236,11 @@ export async function getServerSideProps(context) {
 
     return {
       props: { isConnected: true },
-    }
+    };
   } catch (e) {
-    console.error(e)
+    console.error(e);
     return {
       props: { isConnected: false },
-    }
+    };
   }
 }
---
+++ pages/todo.js
@@ -0,0 +1,214 @@
+import { useState, useCallback } from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import InputGroup from 'react-bootstrap/InputGroup';
+import dbConnect from '../lib/dbConnect';
+import Item from '../models/Item';
+
+const Todo = ({ todos }) => {
+  return (
+    <Container>
+      <Row>
+        <Col md={{ offset: 3, span: 6 }}>
+          <TodoListCard todoItems={todos} />
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+const TodoListCard = ({ todoItems }) => {
+  const [items, setItems] = useState(todoItems);
+
+  const onNewItem = useCallback(
+    (newItem) => {
+      setItems([...items, newItem]);
+    },
+    [items],
+  );
+
+  const onItemUpdate = useCallback(
+    (item) => {
+      const index = items.findIndex((i) => i._id === item._id);
+      setItems([...items.slice(0, index), item, ...items.slice(index + 1)]);
+    },
+    [items],
+  );
+
+  const onItemRemoval = useCallback(
+    (item) => {
+      const index = items.findIndex((i) => i._id === item._id);
+      setItems([...items.slice(0, index), ...items.slice(index + 1)]);
+    },
+    [items],
+  );
+
+  if (items === null) return 'Loading...';
+
+  return (
+    <>
+      <AddItemForm onNewItem={onNewItem} />
+      {items.length === 0 && (
+        <p className='text-center'>No items yet! Add one above!</p>
+      )}
+      {items.map((item) => (
+        <ItemDisplay
+          item={item}
+          key={item._id}
+          onItemUpdate={onItemUpdate}
+          onItemRemoval={onItemRemoval}
+        />
+      ))}
+    </>
+  );
+};
+
+const AddItemForm = ({ onNewItem }) => {
+  const [newItem, setNewItem] = useState('');
+  const [submitting, setSubmitting] = useState(false);
+
+  const submitNewItem = (e) => {
+    e.preventDefault();
+    setSubmitting(true);
+    fetch('/api/items', {
+      method: 'POST',
+      body: JSON.stringify({ name: newItem }),
+      headers: { 'Content-Type': 'application/json' },
+    })
+      .then((r) => r.json())
+      .then((item) => {
+        onNewItem(item.data);
+        setSubmitting(false);
+        setNewItem('');
+      });
+  };
+
+  return (
+    <Form onSubmit={submitNewItem}>
+      <InputGroup className='mb-3'>
+        <Form.Control
+          value={newItem}
+          onChange={(e) => setNewItem(e.target.value)}
+          type='text'
+          placeholder='New Item'
+          aria-describedby='basic-addon1'
+        />
+        <Button
+          type='submit'
+          variant='success'
+          disabled={!newItem.length}
+          className={submitting ? 'disabled' : ''}
+        >
+          {submitting ? 'Adding...' : 'Add Item'}
+        </Button>
+      </InputGroup>
+    </Form>
+  );
+};
+
+const ItemDisplay = ({ item, onItemUpdate, onItemRemoval }) => {
+  const toggleCompletion = () => {
+    fetch(`/api/items/${item._id}`, {
+      method: 'PUT',
+      body: JSON.stringify({
+        name: item.name,
+        completed: !item.completed,
+      }),
+      headers: { 'Content-Type': 'application/json' },
+    })
+      .then((r) => r.json())
+      .then((r) => onItemUpdate(r.data));
+  };
+
+  const removeItem = () => {
+    fetch(`/api/items/${item._id}`, { method: 'DELETE' }).then(() =>
+      onItemRemoval(item),
+    );
+  };
+
+  return (
+    <Container fluid className={`item ${item.completed && 'completed'}`}>
+      <Row>
+        <Col xs={1} className='text-center'>
+          <Button
+            className='toggles'
+            size='sm'
+            variant='link'
+            onClick={toggleCompletion}
+            aria-label={
+              item.completed
+                ? 'Mark item as incomplete'
+                : 'Mark item as complete'
+            }
+          >
+            {item.completed ? (
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                width='16'
+                height='16'
+                fill='currentColor'
+                className='bi bi-check-square'
+                viewBox='0 0 16 16'
+              >
+                <path d='M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z' />
+                <path d='M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.235.235 0 0 1 .02-.022z' />
+              </svg>
+            ) : (
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                width='16'
+                height='16'
+                fill='currentColor'
+                className='bi bi-square'
+                viewBox='0 0 16 16'
+              >
+                <path d='M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z' />
+              </svg>
+            )}
+          </Button>
+        </Col>
+        <Col xs={10} className='name'>
+          {item.name}
+        </Col>
+        <Col xs={1} className='text-center remove'>
+          <Button
+            size='sm'
+            variant='link'
+            onClick={removeItem}
+            aria-label='Remove Item'
+          >
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              width='16'
+              height='16'
+              fill='red'
+              className='bi bi-trash-fill'
+              viewBox='0 0 16 16'
+            >
+              <path d='M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z' />
+            </svg>
+          </Button>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export const getServerSideProps = async () => {
+  await dbConnect();
+
+  /* find all the data in our database */
+  const result = await Item.find({});
+  const items = result.map((doc) => {
+    const item = doc.toObject();
+    item._id = item._id.toString();
+    return item;
+  });
+
+  return { props: { todos: JSON.parse(JSON.stringify(items)) } };
+};
+
+export default Todo;
