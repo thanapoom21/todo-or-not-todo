9aa0b5fbf92dbe5a0f25bbc49a8c247a7bd41afa
thanapoom21 <thanapoom.p.music@gmail.com>
2022-08-12 00:03:28 -0700

    create routes for calculator and clock app and set some style

---
+++ pages/calculator.js
@@ -0,0 +1,261 @@
+import { useState } from "react";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+// Variables
+const isOperator = /[x/+‑]/,
+  endsWithOperator = /[x+‑/]$/,
+  endsWithNegativeSign = /\d[x/+‑]{1}‑$/,
+  clearStyle = { background: '#ac3939' },
+  operatorStyle = { background: '#bbb' },
+  equalsStyle = {
+    background: '#e19200'
+  };
+
+// Main app
+const Calculator = () => {
+  const [currentVal, setCurrentVal] = useState('0');
+  const [prevVal, setPrevVal] = useState('0');
+  const [formula, setFormula] = useState('');
+  const [currentSign, setCurrentSign] = useState('pos');
+  const [lastClicked, setLastClicked] = useState('');
+  const [evaluated, setEvaluated] = useState(false);
+
+  // warn user that they cannot add more digit
+  const maxDigitWarning = () => {
+    setCurrentVal('Digit Limit Met');
+    setPrevVal(currentVal);
+
+    setTimeout(() => {
+      setCurrentVal(prevVal);
+    }, 3000);
+  };
+
+  const handleEvaluate = () => {
+    if (!currentVal.includes('Limit')) {
+      let expression = formula;
+      while (endsWithOperator.test(expression)) {
+        expression = expression.slice(0, -1);
+      }
+      expression = expression
+        .replace(/x/g, '*')
+        .replace(/‑/g, '-')
+        .replace('--', '+0+0+0+0+0+0+');
+
+      // eval() is not recommended and should be replaced with other method.
+      let answer = Math.round(1000000000000 * Function(expression)) / 1000000000000;
+
+      setCurrentVal(answer.toString());
+      setFormula(
+        expression
+          .replace(/\*/g, '⋅')
+          .replace(/-/g, '‑')
+          .replace('+0+0+0+0+0+0+', '‑-')
+          .replace(/(x|\/|\+)‑/, '$1-')
+          .replace(/^‑/, '-') +
+        '=' +
+        answer,
+      );
+      setPrevVal(answer);
+      setEvaluated(true);
+    }
+  };
+
+  const handleOperators = (e) => {
+    if (!currentVal.includes('Limit')) {
+      const value = e.target.value;
+      setCurrentVal(value)
+      setEvaluated(false)
+
+      if (evaluated) {
+        setFormula(prevVal + value)
+      } else if (!endsWithOperator.test(formula)) {
+        setPrevVal(formula);
+        setFormula(formula + value);
+      } else if (!endsWithNegativeSign.test(formula)) {
+        setFormula((endsWithNegativeSign.test(formula + value) ? formula : prevVal) + value);
+      } else if (value !== '‑') {
+        setFormula(prevVal + value)
+      }
+    }
+  }
+
+  const handleNumbers = (e) => {
+    if (!currentVal.includes('Limit')) {
+      const value = e.target.value;
+
+      setEvaluated(false);
+
+      if (currentVal.length > 21) {
+        maxDigitWarning();
+      } else if (evaluated) {
+        setCurrentVal(value);
+        setFormula(value !== '0' ? value : '');
+      } else {
+        setCurrentVal(
+          currentVal === '0' || isOperator.test(currentVal)
+            ? value
+            : currentVal + value,
+        );
+        setFormula(
+          currentVal === '0' && value === '0'
+            ? formula === ''
+              ? value
+              : formula
+            : /([^.0-9]0|^0)$/.test(formula)
+              ? formula.slice(0, -1) + value
+              : formula + value,
+        );
+      }
+    }
+  };
+
+  const handleDecimal = () => {
+    if (evaluated === true) {
+      setCurrentVal('0.');
+      setFormula('0.');
+      setEvaluated(false)
+    } else if (
+      !currentVal.includes('.') && !currentVal.includes('Limit')
+    ) {
+      setEvaluated(false)
+      if (currentVal.length > 21) {
+        maxDigitWarning();
+      } else if (
+        endsWithOperator.test(formula) ||
+        (currentVal === '0' && formula === '')
+      ) {
+        setCurrentVal('0.');
+        setFormula('0.');
+      } else {
+        setCurrentVal(formula.match(/(-?\d+\.?\d*)$/)[0] + '.');
+        setFormula(formula + '.');
+      }
+    }
+  }
+
+  const initialize = () => {
+    setCurrentVal('0');
+    setPrevVal('0');
+    setFormula('');
+    setCurrentSign('pos');
+    setLastClicked('');
+  };
+
+  return (
+    <Container className='d-flex flex-column justify-content-center mt-5'>
+      <Row>
+        <Col md={{ span: 12 }}>
+          <h1>Calculator App</h1>
+          <hr />
+        </Col>
+      </Row>
+      <Row>
+        <Col className="col-md-6 align-self-center">
+          <div className='calculator'>
+            <Formula formula={formula.replace(/x/g, '⋅')} />
+            <Output currentValue={currentVal} />
+            <Buttons
+              decimal={handleDecimal}
+              evaluate={handleEvaluate}
+              initialize={initialize}
+              numbers={handleNumbers}
+              operators={handleOperators}
+            />
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+const Buttons = ({ evaluate, initialize, decimal, numbers, operators }) => {
+  return (
+    <div>
+      <Row className="first-row">
+        <Col className='d-flex flex-row'>
+          <button
+            className='flex-grow-1'
+            id='clear'
+            onClick={initialize}
+            style={clearStyle}
+            value='AC'
+          >
+            AC
+          </button>
+          <button className='flex-grow-0' id='divide' onClick={operators} style={operatorStyle} value='/'>
+            /
+          </button>
+          <button className='flex-grow-0' id='multiply' onClick={operators} style={operatorStyle} value='x'>
+            x
+          </button>
+        </Col>
+      </Row>
+      <Row className="second-row">
+        <Col className='d-flex flex-row'>
+          <button id='seven' onClick={numbers} value='7'>
+            7
+          </button>
+          <button id='eight' onClick={numbers} value='8'>
+            8
+          </button>
+          <button id='nine' onClick={numbers} value='9'>
+            9
+          </button>
+          <button id='subtract' onClick={operators} style={operatorStyle} value='‑'>
+            ‑
+          </button>
+        </Col>
+      </Row>
+      <Row className="third-row">
+        <Col className='d-flex flex-row'>
+          <button id='four' onClick={numbers} value='4'>
+            4
+          </button>
+          <button id='five' onClick={numbers} value='5'>
+            5
+          </button>
+          <button id='six' onClick={numbers} value='6'>
+            6
+          </button>
+          <button id='add' onClick={operators} style={operatorStyle} value='+'>
+            +
+          </button>
+        </Col>
+      </Row>
+      <Row className="fourth-row">
+        <Col className='d-flex flex-row flex-wrap'>
+          <button className="flex-grow-0" id='one' onClick={numbers} value='1'>
+            1
+          </button>
+          <button className="flex-grow-0" id='two' onClick={numbers} value='2'>
+            2
+          </button>
+          <button className="flex-grow-0" id='three' onClick={numbers} value='3'>
+            3
+          </button>
+          <button className="flex-grow-0" id='equals' onClick={evaluate} style={equalsStyle} value='='>
+            =
+          </button>
+          <button className="flex-grow-1" id='zero' onClick={numbers} value='0'>
+            0
+          </button>
+          <button className="flex-grow-0" id='decimal' onClick={decimal} value='.'>
+            .
+          </button>
+        </Col>
+      </Row>
+    </div>
+  );
+};
+
+const Output = ({ currentValue }) => (
+  <div className='outputScreen' id='display'>
+    {currentValue}
+  </div>
+);
+
+const Formula = ({ formula }) => <div className='formulaScreen'>{formula}</div>;
+
+export default Calculator;
---
+++ pages/clock.js
@@ -0,0 +1,275 @@
+import React, { useState } from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+const accurateInterval = (fn, time) => {
+  let nextAt = new Date().getTime() + time;
+  let timeout = null;
+
+  const wrapper = () => {
+    nextAt += time;
+    timeout = setTimeout(wrapper, nextAt - new Date().getTime());
+    return fn();
+  };
+
+  const cancel = () => {
+    return clearTimeout(timeout);
+  };
+
+  timeout = setTimeout(wrapper, nextAt - new Date().getTime());
+
+  return {
+    cancel: cancel,
+  };
+};
+
+// COMPONENTS:
+const TimerLengthControl = ({
+  addID,
+  length,
+  lengthID,
+  minID,
+  onClick,
+  title,
+  titleID,
+}) => {
+  return (
+    <div className='length-control'>
+      <Row>
+        <Col className='d-flex justify-content-between'>
+          <div id={titleID}>{title}</div>
+        </Col>
+        <Col className='d-flex justify-content-between'>
+          <button className='btn-level' id={minID} onClick={onClick} value='-'>
+            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fillRule="currentColor" className="bi bi-arrow-down-circle-fill" viewBox="0 0 16 16">
+              <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V4.5z" />
+            </svg>
+          </button>
+          <div className='btn-level' id={lengthID}>
+            {length}
+          </div>
+          <button className='btn-level' id={addID} onClick={onClick} value='+'>
+            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fillRule="currentColor" className="bi bi-arrow-up-circle-fill" viewBox="0 0 16 16">
+              <path d="M16 8A8 8 0 1 0 0 8a8 8 0 0 0 16 0zm-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z" />
+            </svg>
+          </button>
+        </Col>
+      </Row>
+    </div>
+  );
+};
+
+class Timer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      brkLength: 5,
+      seshLength: 25,
+      timerState: 'stopped',
+      timerType: 'Session',
+      timer: 1500,
+      intervalID: '',
+      alarmColor: { color: 'black' },
+    };
+    this.setBrkLength = this.setBrkLength.bind(this);
+    this.setSeshLength = this.setSeshLength.bind(this);
+    this.lengthControl = this.lengthControl.bind(this);
+    this.timerControl = this.timerControl.bind(this);
+    this.beginCountDown = this.beginCountDown.bind(this);
+    this.decrementTimer = this.decrementTimer.bind(this);
+    this.phaseControl = this.phaseControl.bind(this);
+    this.warning = this.warning.bind(this);
+    this.buzzer = this.buzzer.bind(this);
+    this.switchTimer = this.switchTimer.bind(this);
+    this.clockify = this.clockify.bind(this);
+    this.reset = this.reset.bind(this);
+  }
+  setBrkLength(e) {
+    this.lengthControl(
+      'brkLength',
+      e.currentTarget.value,
+      this.state.brkLength,
+      'Session',
+    );
+  }
+  setSeshLength(e) {
+    this.lengthControl(
+      'seshLength',
+      e.currentTarget.value,
+      this.state.seshLength,
+      'Break',
+    );
+  }
+  lengthControl(stateToChange, sign, currentLength, timerType) {
+    if (this.state.timerState === 'running') {
+      return;
+    }
+    if (this.state.timerType === timerType) {
+      if (sign === '-' && currentLength !== 1) {
+        this.setState({ [stateToChange]: currentLength - 1 });
+      } else if (sign === '+' && currentLength !== 60) {
+        this.setState({ [stateToChange]: currentLength + 1 });
+      }
+    } else if (sign === '-' && currentLength !== 1) {
+      this.setState({
+        [stateToChange]: currentLength - 1,
+        timer: currentLength * 60 - 60,
+      });
+    } else if (sign === '+' && currentLength !== 60) {
+      this.setState({
+        [stateToChange]: currentLength + 1,
+        timer: currentLength * 60 + 60,
+      });
+    }
+  }
+  timerControl() {
+    if (this.state.timerState === 'stopped') {
+      this.beginCountDown();
+      this.setState({ timerState: 'running' });
+    } else {
+      this.setState({ timerState: 'stopped' });
+      if (this.state.intervalID) {
+        this.state.intervalID.cancel();
+      }
+    }
+  }
+  beginCountDown() {
+    this.setState({
+      intervalID: accurateInterval(() => {
+        this.decrementTimer();
+        this.phaseControl();
+      }, 1000),
+    });
+  }
+  decrementTimer() {
+    this.setState({ timer: this.state.timer - 1 });
+  }
+  phaseControl() {
+    let timer = this.state.timer;
+    this.warning(timer);
+    this.buzzer(timer);
+    if (timer < 0) {
+      if (this.state.intervalID) {
+        this.state.intervalID.cancel();
+      }
+      if (this.state.timerType === 'Session') {
+        this.beginCountDown();
+        this.switchTimer(this.state.brkLength * 60, 'Break');
+      } else {
+        this.beginCountDown();
+        this.switchTimer(this.state.seshLength * 60, 'Session');
+      }
+    }
+  }
+  warning(_timer) {
+    if (_timer < 61) {
+      this.setState({ alarmColor: { color: '#a50d0d' } });
+    } else {
+      this.setState({ alarmColor: { color: 'orange' } });
+    }
+  }
+  buzzer(_timer) {
+    if (_timer === 0) {
+      this.audioBeep.play();
+    }
+  }
+  switchTimer(num, str) {
+    this.setState({
+      timer: num,
+      timerType: str,
+      alarmColor: { color: 'red' },
+    });
+  }
+  clockify() {
+    let minutes = Math.floor(this.state.timer / 60);
+    let seconds = this.state.timer - minutes * 60;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    return minutes + ':' + seconds;
+  }
+  reset() {
+    this.setState({
+      brkLength: 5,
+      seshLength: 25,
+      timerState: 'stopped',
+      timerType: 'Session',
+      timer: 1500,
+      intervalID: '',
+      alarmColor: { color: 'black' },
+    });
+    if (this.state.intervalID) {
+      this.state.intervalID.cancel();
+    }
+    this.audioBeep.pause();
+    this.audioBeep.currentTime = 0;
+  }
+  render() {
+    return (
+      <Container id="clockapp" className='d-flex flex-column justify-content-center mt-5'>
+        <Row>
+          <Col md={{ span: 12 }}>
+            <h1>Clock App</h1>
+            <hr />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <h2 className='main-title'>25 + 5 Clock</h2>
+            <TimerLengthControl
+              addID='break-increment'
+              length={this.state.brkLength}
+              lengthID='break-length'
+              minID='break-decrement'
+              onClick={this.setBrkLength}
+              title='Break Length'
+              titleID='break-label'
+            />
+            <TimerLengthControl
+              addID='session-increment'
+              length={this.state.seshLength}
+              lengthID='session-length'
+              minID='session-decrement'
+              onClick={this.setSeshLength}
+              title='Session Length'
+              titleID='session-label'
+            />
+            <div className='timer my-4' style={this.state.alarmColor}>
+              <div className='timer-wrapper d-flex flex-column justify-content-center align-items-center'>
+                <div id='timer-label'>{this.state.timerType}</div>
+                <div id='time-left'>{this.clockify()}</div>
+              </div>
+            </div>
+            <div className='timer-control'>
+              <button id='start_stop' onClick={this.timerControl}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fillRule="currentColor" className="bi bi-play-circle" viewBox="0 0 16 16">
+                  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                  <path d="M6.271 5.055a.5.5 0 0 1 .52.038l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 6 10.5v-5a.5.5 0 0 1 .271-.445z" />
+                </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fillRule="currentColor" className="bi bi-pause-circle" viewBox="0 0 16 16">
+                  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                  <path d="M5 6.25a1.25 1.25 0 1 1 2.5 0v3.5a1.25 1.25 0 1 1-2.5 0v-3.5zm3.5 0a1.25 1.25 0 1 1 2.5 0v3.5a1.25 1.25 0 1 1-2.5 0v-3.5z" />
+                </svg>
+              </button>
+              <button id='reset' onClick={this.reset}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fillRule="currentColor" className="bi bi-eraser" viewBox="0 0 16 16">
+                  <path d="M8.086 2.207a2 2 0 0 1 2.828 0l3.879 3.879a2 2 0 0 1 0 2.828l-5.5 5.5A2 2 0 0 1 7.879 15H5.12a2 2 0 0 1-1.414-.586l-2.5-2.5a2 2 0 0 1 0-2.828l6.879-6.879zm2.121.707a1 1 0 0 0-1.414 0L4.16 7.547l5.293 5.293 4.633-4.633a1 1 0 0 0 0-1.414l-3.879-3.879zM8.746 13.547 3.453 8.254 1.914 9.793a1 1 0 0 0 0 1.414l2.5 2.5a1 1 0 0 0 .707.293H7.88a1 1 0 0 0 .707-.293l.16-.16z" />
+                </svg>
+              </button>
+            </div>
+            <audio
+              id='beep'
+              preload='auto'
+              ref={(audio) => {
+                this.audioBeep = audio;
+              }}
+              src='https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav'
+            />
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
+
+export default Timer;
