59296ec09caf4172614b57a392bb844dfb069512
thanapoom21 <thanapoom.p.music@gmail.com>
2022-09-29 22:33:48 -0700

    change layout and modify button color

--- pages/[id]/index.js
+++ pages/[id]/index.js
@@ -3,6 +3,11 @@ import { useRouter } from 'next/router';
 import Link from 'next/link';
 import dbConnect from '../../lib/dbConnect';
 import Pet from '../../models/Pet';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
 /* Allows you to view pet card info and delete pet card*/
 const PetPage = ({ pet }) => {
@@ -22,44 +27,58 @@ const PetPage = ({ pet }) => {
   };
 
   return (
-    <div key={pet._id}>
-      <div className='card'>
-        <img src={pet.image_url} />
-        <h5 className='pet-name'>{pet.name}</h5>
-        <div className='main-content'>
-          <p className='pet-name'>{pet.name}</p>
-          <p className='owner'>Owner: {pet.owner_name}</p>
+    <Container className='d-flex flex-column justify-content-center mt-5'>
+      <Row>
+        <Col></Col>
+        <Col xs={8}>
+          <Card key={pet._id}>
+            <Card.Img variant='top' src={pet.image_url} />
+            <Card.Body>
+              <Card.Title className='pet-name'>Name: {pet.name}</Card.Title>
+              <div className='main-content'>
+                <p className='owner'>Owner: {pet.owner_name}</p>
 
-          {/* Extra Pet Info: Likes and Dislikes */}
-          <div className='likes info'>
-            <p className='label'>Likes</p>
-            <ul>
-              {pet.likes.map((data, index) => (
-                <li key={index}>{data} </li>
-              ))}
-            </ul>
-          </div>
-          <div className='dislikes info'>
-            <p className='label'>Dislikes</p>
-            <ul>
-              {pet.dislikes.map((data, index) => (
-                <li key={index}>{data} </li>
-              ))}
-            </ul>
-          </div>
+                {/* Extra Pet Info: Likes and Dislikes */}
+                <div className='likes info'>
+                  <p className='label'>Likes</p>
+                  <ul>
+                    {pet.likes.map((data, index) => (
+                      <li key={index}>{data} </li>
+                    ))}
+                  </ul>
+                </div>
+                <div className='dislikes info'>
+                  <p className='label'>Dislikes</p>
+                  <ul>
+                    {pet.dislikes.map((data, index) => (
+                      <li key={index}>{data} </li>
+                    ))}
+                  </ul>
+                </div>
 
-          <div className='btn-container'>
-            <Link href='/[id]/edit' as={`/${pet._id}/edit`}>
-              <button className='btn edit'>Edit</button>
-            </Link>
-            <button className='btn delete' onClick={handleDelete}>
-              Delete
-            </button>
-          </div>
-        </div>
-      </div>
-      {message && <p>{message}</p>}
-    </div>
+                <div className='btn-container d-flex justify-content-between'>
+                  <Link href='/[id]/edit' as={`/${pet._id}/edit`}>
+                    <Button variant='outline-danger' className='btn edit px-4'>
+                      Edit
+                    </Button>
+                  </Link>
+
+                  <Button
+                    variant='danger'
+                    className='btn delete px-4'
+                    onClick={handleDelete}
+                  >
+                    Delete
+                  </Button>
+                </div>
+              </div>
+            </Card.Body>
+            {message && <p>{message}</p>}
+          </Card>
+        </Col>
+        <Col></Col>
+      </Row>
+    </Container>
   );
 };
 
