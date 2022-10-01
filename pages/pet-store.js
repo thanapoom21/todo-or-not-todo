05135cadca6a49747c116b1bb0988e7b6365289a
thanapoom21 <thanapoom.p.music@gmail.com>
2022-10-01 01:45:12 -0700

    implement conditional rendering when there are too many pets

--- pages/pet-store.js
+++ pages/pet-store.js
@@ -17,62 +17,90 @@ const PetStore = ({ pets }) => {
             <hr />
           </Col>
         </Row>
-        <Row>
-          <Col>
-            <Link href="/new">
-              <Button variant='outline-primary'>
-                Add New Pet
-              </Button>
-            </Link>
-          </Col>
-        </Row>
-        <Row xs={1} md={2} xl={4} className='my-2 gy-2'>
-          {/* Create a card for each pet */}
-          {pets.map((pet) => (
-            <Col key={pet._id}>
-              <Card>
-                <Card.Img variant='top' src={pet.image_url} />
-                <Card.Body>
-                  <Card.Title className='pet-name'>Name: {pet.name}</Card.Title>
-                  <div className='main-content'>
-                    <p className='owner'>Owner: {pet.owner_name}</p>
 
-                    {/* Extra Pet Info: Likes and Dislikes */}
-                    <div className='likes info'>
-                      <p className='label'>Likes</p>
-                      <ul>
-                        {pet.likes.map((data, index) => (
-                          <li key={index}>{data} </li>
-                        ))}
-                      </ul>
-                    </div>
-                    <div className='dislikes info'>
-                      <p className='label'>Dislikes</p>
-                      <ul>
-                        {pet.dislikes.map((data, index) => (
-                          <li key={index}>{data} </li>
-                        ))}
-                      </ul>
-                    </div>
+        {pets.length > 8 ? (
+          <>
+            <Row>
+              <Col>
+                <Link href='/new'>
+                  <Button variant='outline-secondary' disabled>
+                    Add New Pet
+                  </Button>
+                </Link>
+              </Col>
+            </Row>
+            <Row className='my-2 gy-2'>
+              <Col md={{ span: 12 }}>
+                <h2>
+                  Sorry for the inconvenience, our store is currently full.
+                </h2>
+              </Col>
+            </Row>
+          </>
+        ) : (
+          <>
+            <Row>
+              <Col>
+                <Link href='/new'>
+                  <Button variant='outline-primary'>Add New Pet</Button>
+                </Link>
+              </Col>
+            </Row>
 
-                    <div className='btn-container d-flex justify-content-between'>
-                      <Link href='/[id]/edit' as={`/${pet._id}/edit`}>
-                        <Button variant='outline-danger' className='btn edit px-4'>
-                          Edit
-                        </Button>
-                      </Link>
-                      <Link href='/[id]' as={`/${pet._id}`}>
-                        <Button variant='primary' className='btn view px-4'>
-                          View
-                        </Button>
-                      </Link>
-                    </div>
-                  </div>
-                </Card.Body>
-              </Card>
-            </Col>
-          ))}
-        </Row>
+            <Row xs={1} md={2} xl={4} className='my-2 gy-2'>
+              {/* Create a card for each pet */}
+              {pets.map((pet) => (
+                <Col key={pet._id}>
+                  <Card>
+                    <Card.Img variant='top' src={pet.image_url} />
+                    <Card.Body>
+                      <Card.Title className='pet-name'>
+                        Name: {pet.name}
+                      </Card.Title>
+                      <div className='main-content'>
+                        <p className='owner'>Owner: {pet.owner_name}</p>
+
+                        {/* Extra Pet Info: Likes and Dislikes */}
+                        <div className='likes info'>
+                          <p className='label'>Likes</p>
+                          <ul>
+                            {pet.likes.map((data, index) => (
+                              <li key={index}>{data} </li>
+                            ))}
+                          </ul>
+                        </div>
+                        <div className='dislikes info'>
+                          <p className='label'>Dislikes</p>
+                          <ul>
+                            {pet.dislikes.map((data, index) => (
+                              <li key={index}>{data} </li>
+                            ))}
+                          </ul>
+                        </div>
+
+                        <div className='btn-container d-flex justify-content-between'>
+                          <Link href='/[id]/edit' as={`/${pet._id}/edit`}>
+                            <Button
+                              variant='outline-danger'
+                              className='btn edit px-4'
+                            >
+                              Edit
+                            </Button>
+                          </Link>
+                          <Link href='/[id]' as={`/${pet._id}`}>
+                            <Button variant='primary' className='btn view px-4'>
+                              View
+                            </Button>
+                          </Link>
+                        </div>
+                      </div>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              ))}
+            </Row>
+          </>
+        )}
       </Container>
     </>
   );
